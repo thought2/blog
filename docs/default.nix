99a80ae571c70a5a54e18ece027f6273bffa1d70
Michael Bock <me@thought2.de>
2019-09-10 12:10:08 +0200

    Change config

--- docs/default.nix
+++ docs/default.nix
@@ -1,13 +1,22 @@
 with (import <nixpkgs> {});
 let
-  env = bundlerEnv {
-    name = "your-package";
+  jekyll_env = bundlerEnv {
+    name = "blog";
     inherit ruby;
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
   };
 in stdenv.mkDerivation {
-  name = "your-package";
-  buildInputs = [env ruby];
+  name = "blog";
+  buildInputs = [jekyll_env ruby];
+
+  src = ./.;
+
+  phases = ["installPhase"];
+
+  installPhase = ''
+    mkdir -p $out
+    jekyll build -s $src -b blog -d $out
+  '';
 }
